b60954faeaf73c88202bf4895bd2132bee3b6c13
Breno M <breno.mm@outlook.com>
2025-06-23 22:40:42 -0300

    feat: add dockerfile and docker compose file

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLITE_URL = "sqlite:///./app.db"
+SQLITE_URL = "sqlite:///./data/app.db"
 
 engine = create_engine(SQLITE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
